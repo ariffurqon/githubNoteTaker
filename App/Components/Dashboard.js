97fde24f54b68b801ada65e2cb0f56ddb66f2e98
ariffurqon <arif.furqon@gmail.com>
2015-12-11 00:56:04 -0800

    finish dashboard, reusable component and finishing profile

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -1,35 +1,65 @@
 var React = require('react-native');
+var Profile = require('./Profile');
 
 var {
 	Text,
 	View,
-	StyleSheet
+	StyleSheet,
+	Image,
+	TouchableHighlight
 } = React;
 
 class Dashboard extends React.Component{
-	render(){
-		return (
-			<View style={styles.container}>
-				<Text> This is the Dashboard </Text>
-				<Text> {this.props.userInfo} </Text>
-			</View>
-		)
-	}
-};
-
-var styles = StyleSheet.create({
-  container: {
-    marginTop: 65,
-    flex: 1
-  },
-  image: {
-    height: 350,
-  },
-  buttonText: {
-    fontSize: 24,
-    color: 'white',
-    alignSelf: 'center'
+   makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+    if(btn === 0){
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1){
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
   }
-});
+  goToProfile(){
+    console.log('Going to Profile Page');
+  }
+  goToRepos(){
+    console.log('Going to Repos');
+  }
+  goToNotes(){
+  console.log('Going to Notes');
+  }
+render(){
+  return (
+      <View style={styles.container}>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
+        <TouchableHighlight
+            style={this.makeBackground(0)}
+            onPress={this.goToProfile.bind(this)}
+            underlayColor="#88D4F5">
+              <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(1)}
+            onPress={this.goToRepos.bind(this)}
+            underlayColor="#E39EBF">
+              <Text style={styles.buttonText}>View Repositories</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(2)}
+            onPress={this.goToNotes.bind(this)}
+            underlayColor="#9BAAF3">
+              <Text style={styles.buttonText}>Take Notes</Text>
+        </TouchableHighlight>
+      </View>
+    )
+}
+};
 
 module.exports = Dashboard;
--- App/Components/Main.js
+++ App/Components/Main.js
@@ -12,79 +12,70 @@ var{
 } = React;
 
 class Main extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			username: '',
-			isLoading: false,
-			error: false
-		}
-	}
-
-	handleChange(event){
-		this.setState({
-			username: event.nativeEvent.text
-		})
-	}
-
-	handleSubmit(){
-		//update our indicatorIOS spinner
-		this.setState({
-			isLoading: true
-		});
-		// console.log('SUBMIT', this.state.username);
-		//fetch data from github
-		api.getBio(this.state.username)
-			.then((res) => {
-				if(res.message === 'not found'){
-					this.setState({
-						error: 'User not found',
-						isLoading: false
-					})
-				} else {
-					this.props.navigator.push({
-						title: res.name || "Select an Option",
-						component: Dashboard,
-						passProps: {userInfo: res}
-					});
-					this.setState({
-						isLoading: false,
-						error: false,
-						username: ''
-					});
-				}
-			});
-		//reroute to the next passing that github information
-	}
-
-
-	render(){
-		var showErr = (
-		    this.state.error ? <Text> {this.state.error} </Text> : <View></View>
-		);
-		return (
-		      <View style={styles.mainContainer}>
-		        <Text style={styles.title}>
-		          Search for a Github User
-		        </Text>
-		        <TextInput
-		          style={styles.searchInput}
-		          value={this.state.username}
-		          onChange={this.handleChange.bind(this)} />
-		        <TouchableHighlight
-		          style={styles.button}
-		          onPress={this.handleSubmit.bind(this)}
-		          underlayColor="white">
-		            <Text style={styles.buttonText}>SEARCH</Text>
-		        </TouchableHighlight>
-		        <ActivityIndicatorIOS
-		          animating={this.state.isLoading}
-		          color="#111"
-		          size="large" />
-		        {showErr}
-		      </View>
-		);
-	}
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      isLoading: false,
+      error: false
+    }
+  }
+  handleChange(event){
+    this.setState({
+      username: event.nativeEvent.text
+    })
+  }
+  handleSubmit(){
+    // update our indicatorIOS spinner
+    this.setState({
+      isLoading: true
+    });
+    api.getBio(this.state.username)
+      .then((res) => {
+        if(res.message === 'Not Found'){
+          this.setState({
+            error: 'User not found',
+            isLoading: false
+          })
+        } else {
+          this.props.navigator.push({
+            title: res.name || "Select an Option",
+            component: Dashboard,
+            passProps: {userInfo: res}
+          });
+          this.setState({
+            isLoading: false,
+            error: false,
+            username: ''
+          })
+        }
+      });
+  }
+  render() {
+    var showErr = (
+      this.state.error ? <Text> {this.state.error} </Text> : <View></View>
+    );
+    return(
+      <View style={styles.mainContainer}>
+      <Text style={styles.title}> Search for a Github User </Text>
+      <TextInput
+        style={styles.searchInput}
+        value={this.state.username}
+        onChange={this.handleChange.bind(this)} />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="white">
+          <Text style={styles.buttonText}> SEARCH </Text>
+        </TouchableHighlight>
+        <ActivityIndicatorIOS
+          animating={this.state.isLoading}
+          color="#111"
+          size="large"></ActivityIndicatorIOS>
+        {showErr}
+      </View>
+      )
+  }
 };
 
 var styles = StyleSheet.create({
