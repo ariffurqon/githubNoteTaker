8243348f0b78da8dafead03b33fc1d4c0bee4f80
ariffurqon <arif.furqon@gmail.com>
2015-12-10 22:35:59 -0800

    added state and touch events

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -3,18 +3,59 @@ var React = require('react-native');
 var{
 	View,
 	Text,
-	StyleSheet
+	StyleSheet,
+	TextInput, 
+	TouchableHighlight,
+	ActivityIndicatorIOS
 } = React;
 
 class Main extends React.Component{
-	render(){
-		return(
-			<View style={styles.mainContainer}>
-				<Text>Testing the Router..</Text>	
-			</View>
-		)
+	constructor(props){
+		super(props);
+		this.state = {
+			username: '',
+			isloading: false,
+			error: false
+		}
 	}
-}
+
+	handleChange(event){
+		this.setState({
+			username: event.nativeEvent.text
+		});
+	}
+
+	handleSubmit(){
+		//update our indicatorIOS spinner
+		this.setState({
+			isloading: true
+		});
+		console.log('SUBMIT', this.state.username);
+		//fetch data from github
+		//reroute to the next passing that github information
+	}
+
+
+	render(){
+		return (
+		    <View style={styles.mainContainer}>
+		        <Text style={styles.title}>
+		          Search for a Github User
+		        </Text>
+		        <TextInput
+		          style={styles.searchInput}
+		          value={this.state.username}
+		          onChange={this.handleChange.bind(this)} />
+		        <TouchableHighlight
+		          style={styles.button}
+		          onPress={this.handleSubmit.bind(this)}
+		          underlayColor="white">
+		            <Text style={styles.buttonText}>SEARCH</Text>
+		        </TouchableHighlight>
+			</View>
+		);
+	}
+};
 
 var styles = StyleSheet.create({
 	mainContainer: {
@@ -31,6 +72,35 @@ var styles = StyleSheet.create({
 		textAlign: 'center',
 		color: '#ffff'
 	},
+	searchInput: {
+	    height: 50,
+	    padding: 4,
+	    marginRight: 5,
+	    fontSize: 23,
+	    borderWidth: 1,
+	    borderColor: 'white',
+	    borderRadius: 8,
+	    color: 'white'
+	  },
+	  buttonText: {
+	    fontSize: 18,
+	    color: '#111',
+	    alignSelf: 'center'
+	  },
+	  button: {
+	    height: 45,
+	    flexDirection: 'row',
+	    backgroundColor: 'white',
+	    borderColor: 'white',
+	    borderWidth: 1,
+	    borderRadius: 8,
+	    marginBottom: 10,
+	    marginTop: 10,
+	    alignSelf: 'stretch',
+	    justifyContent: 'center'
+	  },
+
+	
 });
 
 module.exports = Main;
